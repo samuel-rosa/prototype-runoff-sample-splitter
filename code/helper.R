bf4fd46eaa9070f0b2df469275e05d8e4a287b17
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-10-06 22:09:52 -0300

    função para computar o teste t para a média de uma amostra

--- code/helper.R
+++ code/helper.R
@@ -5,3 +5,10 @@ my_reshape <- function (x, y) {
   colnames(x) <- colnames(y)
   x
 }
+# Função para computar o teste t para a média de uma amostra
+teste_t_uma <- 
+  function (d) {
+    group_by(d, entrada, saida) %>% 
+      summarise('*t*' = t.test(erro, mu = 0, alternative = "two.sided")$statistic,
+                '*P*' = t.test(erro, mu = 0, alternative = "two.sided")$p.value)
+  }
