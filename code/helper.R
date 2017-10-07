2a0583671eb619b41d076c934d9521195f3c6793
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-10-06 23:34:59 -0300

    tabela do teste do volume de água do fracionador

--- code/helper.R
+++ code/helper.R
@@ -6,7 +6,7 @@ my_reshape <- function (x, y) {
   x
 }
 # Função para computar o teste t para a média de uma amostra
-teste_t_uma <- 
+teste_t1 <- 
   function (d) {
     group_by(d, entrada, saida) %>% 
       summarise('*t*' = t.test(erro, mu = 0, alternative = "two.sided")$statistic,
