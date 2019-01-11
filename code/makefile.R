db22197e5ecc29158f8db5fbc1a2a2f4b03cc348
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-01-10 22:17:57 -0200

    creates makefile

---
+++ code/makefile.R
@@ -0,0 +1,6 @@
+# The following code chunk will render the document
+rmarkdown::render('code/main.Rmd', encoding = 'UTF-8', output_dir = "docs")
+
+# The following code chunk will update the exiting document on Google Drive
+id <- "1l09a5x98YTJmME1DMojzlkXqkAZjCiLy1rKzFCe5sgU"
+googledrive::drive_update(file = googledrive::as_id(id), media = "docs/main.docx")
