cf38354a862bfe4884ce2def34661ab71a870704
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-02-02 12:14:18 -0200

    uploads HTML to Google Drive; equations are rendered as figures

--- code/makefile.R
+++ code/makefile.R
@@ -3,4 +3,4 @@ rmarkdown::render('code/main.Rmd', encoding = 'UTF-8', output_dir = "docs")
 
 # The following code chunk will update the exiting document on Google Drive
 id <- "1l09a5x98YTJmME1DMojzlkXqkAZjCiLy1rKzFCe5sgU"
-googledrive::drive_update(file = googledrive::as_id(id), media = "docs/main.docx")
+googledrive::drive_update(file = googledrive::as_id(id), media = "docs/main.html")
