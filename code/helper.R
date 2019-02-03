a98ce11544c224b7b138aa04c495b6329ba01bc1
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2019-02-03 18:06:37 -0200

    translates results, discussion and conclusion

--- code/helper.R
+++ code/helper.R
@@ -18,12 +18,12 @@ teste_t1 <-
   function (d, alternative = "two.sided", fracionador = TRUE) {
     if (fracionador) {
       group_by(d, entrada, saida) %>% 
-        summarise('*t*' = t.test(erro, mu = 0, alternative = alternative)$statistic %>% round(3),
-                  '*P*' = t.test(erro, mu = 0, alternative = alternative)$p.value %>% round(4))
+        summarise('_t_~1~' = t.test(erro, mu = 0, alternative = alternative)$statistic %>% round(3),
+                  '_P_~1~' = t.test(erro, mu = 0, alternative = alternative)$p.value %>% round(4))
     } else {
       group_by(d, entrada) %>% 
-        summarise('*t*~1~' = t.test(erro, mu = 0, alternative = "two.sided")$statistic,
-                  '*P*~1~' = t.test(erro, mu = 0, alternative = "two.sided")$p.value) 
+        summarise('_t_~1~' = t.test(erro, mu = 0, alternative = "two.sided")$statistic,
+                  '_P_~1~' = t.test(erro, mu = 0, alternative = "two.sided")$p.value) 
     }
   }
 # Função para computar o teste t para a média de duas amostras pareadas
@@ -36,8 +36,8 @@ teste_t1 <-
       tidyr::spread(saida, as.character(y))
     names(tmp) <- gsub(" ", "", names(tmp))
     summarise(tmp,
-              '*t*~2~' = t.test(A, B, mu = 0, alternative = "two.sided", paired = TRUE)$statistic %>% round(3),
-              '*P*~2~' = t.test(A, B, mu = 0, alternative = "two.sided", paired = TRUE)$p.value %>% round(4))
+              '_t_~2~' = t.test(A, B, mu = 0, alternative = "two.sided", paired = TRUE)$statistic %>% round(3),
+              '_P_~2~' = t.test(A, B, mu = 0, alternative = "two.sided", paired = TRUE)$p.value %>% round(4))
   }
 teste_t2 <-
   function (x = d) {
@@ -46,7 +46,7 @@ teste_t2 <-
     t2 <- t2[, rep(1:nrow(i), each = 2)] %>% 
       apply(2, as.numeric) %>% 
       as.data.frame()
-    cbind(r = c('*t*~2~', '*P*~2~'), t2)
+    cbind(r = c('_t_~2~', '_P_~2~'), t2)
   }
 # Função auxiliar para preparar dados para análise
 preparar_dados <- 
--- code/makefile.R
+++ code/makefile.R
@@ -1,5 +1,5 @@
 # The following code chunk will render the document
-rmarkdown::render('code/main.Rmd', encoding = 'UTF-8', output_dir = "docs")
+rmarkdown::render(input = 'code/main.Rmd', encoding = 'UTF-8', output_dir = "docs")
 
 # The following code chunk will update the exiting document on Google Drive
 id <- "1l09a5x98YTJmME1DMojzlkXqkAZjCiLy1rKzFCe5sgU"
