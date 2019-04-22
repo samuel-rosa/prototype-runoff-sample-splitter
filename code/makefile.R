3490648d3ed76e962a707af3c86d1e22ead7a38b
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-04-22 11:46:01 -0300

    does not upload to GD anymore

--- code/makefile.R
+++ code/makefile.R
@@ -2,5 +2,5 @@
 rmarkdown::render(input = 'code/main.Rmd', encoding = 'UTF-8', output_dir = "docs")
 
 # The following code chunk will update the exiting document on Google Drive
-id <- "1l09a5x98YTJmME1DMojzlkXqkAZjCiLy1rKzFCe5sgU"
+# id <- "1l09a5x98YTJmME1DMojzlkXqkAZjCiLy1rKzFCe5sgU"
 # googledrive::drive_update(file = googledrive::as_id(id), media = "docs/main.html")
